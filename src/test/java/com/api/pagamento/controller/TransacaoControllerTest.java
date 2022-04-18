8546226f2a3e8275b123869321d5dde8a12e1767
Euller Henrique <eullerhenrique@outlook.com>
2022-04-18 15:11:45 -0300

    Update

--- src/test/java/com/api/pagamento/controller/TransacaoControllerTest.java
+++ src/test/java/com/api/pagamento/controller/TransacaoControllerTest.java
@@ -19,8 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +28,20 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/*
+
+    TDD é uma sigla para Test Driven Development, ou Desenvolvimento Orientado a Testes. A ideia do TDD é que
+    você trabalhe em ciclos. Estes ciclos ocorrem na seguinte ordem:
+
+    Primeiro, escreva um teste unitário que inicialmente irá falhar, tendo em vista que o código ainda não foi implementado;
+    Crie o código que satisfaça esse teste, ou seja: implemente a funcionalidade em questão. Essa primeira implementação
+    deverá satisfazer imediatamente o teste que foi escrito no ciclo anterior;
+    Quando o código estiver implementado e o teste satisfeito, refatore o código para melhorar pontos como legibilidade.
+    Logo após, execute o teste novamente. A nova versão do código também deverá passar sem que seja necessário modificar o teste escrito inicialmente.
+
+
+ */
 
 /*
 
@@ -113,7 +125,7 @@ public class TransacaoControllerTest {
 
     //When: Após um mock ser criado, você pode direcionar um retorno para um metodo dado um parametro de entrada.
 
-    // Quando a transação é chamada, ela deve ser criada
+    // Quando o pagamento é chamado, ele deve ser criado
     @Test
     void whenPaymentIsCalledThenItShouldBeCreated() throws Exception {
 
--- src/test/java/com/api/pagamento/service/TransacaoServiceTest.java
+++ src/test/java/com/api/pagamento/service/TransacaoServiceTest.java
@@ -16,12 +16,29 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.validation.ConstraintViolationException;
 
-import static org.hamcrest.Matchers.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+/*
+
+    TDD é uma sigla para Test Driven Development, ou Desenvolvimento Orientado a Testes. A ideia do TDD é que
+    você trabalhe em ciclos. Estes ciclos ocorrem na seguinte ordem:
+
+    Primeiro, escreva um teste unitário que inicialmente irá falhar, tendo em vista que o código ainda não foi implementado;
+    Crie o código que satisfaça esse teste, ou seja: implemente a funcionalidade em questão. Essa primeira implementação
+    deverá satisfazer imediatamente o teste que foi escrito no ciclo anterior;
+    Quando o código estiver implementado e o teste satisfeito, refatore o código para melhorar pontos como legibilidade.
+    Logo após, execute o teste novamente. A nova versão do código também deverá passar sem que seja necessário modificar o teste escrito inicialmente.
+
+
+ */
 
 /*
 
@@ -95,17 +112,17 @@ public class TransacaoServiceTest {
 
     //When: Após um mock ser criado, você pode direcionar um retorno para um metodo dado um parametro de entrada.
 
-    // Quando a transação é informada, ela deve ser criada
+    // Quando o pagamento é informado, ele deve ser criado
     @Test
-    void whenTransactionInformedThenItShouldBeCreated() throws InsercaoNaoPermitidaException {
+    void whenPaymentInformedThenItShouldBeCreated() throws InsercaoNaoPermitidaException {
 
         // Dado
 
             //Gera um TransacaoDTO
-            TransacaoDTO transacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
+            TransacaoDTO expectedTransacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
 
             //Tranforma o TransacaoDTO em um Transacao
-            Transacao transacao = (Transacao) Mapper.convert(transacaoDTO, Transacao.class);
+            Transacao transacao = (Transacao) Mapper.convert(expectedTransacaoDTO, Transacao.class);
 
         //Quando
 
@@ -119,16 +136,16 @@ public class TransacaoServiceTest {
             //Cria um TransacaoDTO
             TransacaoDTO createdTransacaoDTO  = transacaoService.pagar(transacao);
 
-            //Verifica se o atributo id do createdBeerDTO é igual ao atributo id do expectedDTO
-            assertThat(createdTransacaoDTO.getId(), is(equalTo(transacaoDTO.getId())));
-            assertThat(createdTransacaoDTO.getCartao(), is(equalTo(transacaoDTO.getCartao())));
-            assertThat(createdTransacaoDTO.getDescricao().getId(), is(equalTo(transacaoDTO.getDescricao().getId())));
-            assertThat(createdTransacaoDTO.getDescricao().getValor(), is(equalTo(transacaoDTO.getDescricao().getValor())));
-            assertThat(createdTransacaoDTO.getDescricao().getDataHora(), is(equalTo(transacaoDTO.getDescricao().getDataHora())));
-            assertThat(createdTransacaoDTO.getDescricao().getEstabelecimento(), is(equalTo(transacaoDTO.getDescricao().getEstabelecimento())));
-            assertThat(createdTransacaoDTO.getFormaPagamento().getId(), is(equalTo(transacaoDTO.getFormaPagamento().getId())));
-            assertThat(createdTransacaoDTO.getFormaPagamento().getTipo(), is(equalTo(transacaoDTO.getFormaPagamento().getTipo())));
-            assertThat(createdTransacaoDTO.getFormaPagamento().getParcelas(), is(equalTo(transacaoDTO.getFormaPagamento().getParcelas())));
+            //Verifica se o atributo id do createdTransacaoDTO é igual ao atributo id do expectedDTO
+            assertThat(createdTransacaoDTO.getId(), is(equalTo(expectedTransacaoDTO.getId())));
+            assertThat(createdTransacaoDTO.getCartao(), is(equalTo(expectedTransacaoDTO.getCartao())));
+            assertThat(createdTransacaoDTO.getDescricao().getId(), is(equalTo(expectedTransacaoDTO.getDescricao().getId())));
+            assertThat(createdTransacaoDTO.getDescricao().getValor(), is(equalTo(expectedTransacaoDTO.getDescricao().getValor())));
+            assertThat(createdTransacaoDTO.getDescricao().getDataHora(), is(equalTo(expectedTransacaoDTO.getDescricao().getDataHora())));
+            assertThat(createdTransacaoDTO.getDescricao().getEstabelecimento(), is(equalTo(expectedTransacaoDTO.getDescricao().getEstabelecimento())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getId(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getId())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getTipo(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getTipo())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getParcelas(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getParcelas())));
 
     }
 
@@ -205,6 +222,161 @@ public class TransacaoServiceTest {
 
              //Verifica se transacaoService.pagar(transacao) lançou a exceção ConstraintViolationException.class
             assertThrows(ConstraintViolationException.class, () -> transacaoService.pagar(transacao));
+
+    }
+
+    // Quando estorno é chamado pelo id, o estorno é retornado
+    @Test
+    void whenReversalInformedByIdThenReversalIsReturned() throws Exception {
+
+        // Dado
+
+        Long id = 1L;
+
+        //Gera um TransacaoDTO
+        TransacaoDTO expectedTransacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
+
+        expectedTransacaoDTO.getDescricao().setNsu("1234567890");
+        expectedTransacaoDTO.getDescricao().setCodigoAutorizacao("147258369");
+        expectedTransacaoDTO.getDescricao().setStatus(StatusEnum.NEGADO);
+
+        //Tranforma o TransacaoDTO em um Transacao
+        Transacao transacao = (Transacao) Mapper.convert(expectedTransacaoDTO, Transacao.class);
+
+        //When
+
+        //transacaoService.findById(id) -> transacao
+        when(transacaoRepository.findById(id))
+                .thenReturn(Optional.ofNullable(transacao));
+
+        // Então
+
+          //Cria um TransacaoDTO
+            TransacaoDTO createdTransacaoDTO  = transacaoService.estornar(id);
+
+            //Verifica se o atributo id do createdTransacaoDTO é igual ao atributo id do expectedDTO
+            assertThat(createdTransacaoDTO.getId(), is(equalTo(expectedTransacaoDTO.getId())));
+            assertThat(createdTransacaoDTO.getCartao(), is(equalTo(expectedTransacaoDTO.getCartao())));
+            assertThat(createdTransacaoDTO.getDescricao().getId(), is(equalTo(expectedTransacaoDTO.getDescricao().getId())));
+            assertThat(createdTransacaoDTO.getDescricao().getValor(), is(equalTo(expectedTransacaoDTO.getDescricao().getValor())));
+            assertThat(createdTransacaoDTO.getDescricao().getDataHora(), is(equalTo(expectedTransacaoDTO.getDescricao().getDataHora())));
+            assertThat(createdTransacaoDTO.getDescricao().getEstabelecimento(), is(equalTo(expectedTransacaoDTO.getDescricao().getEstabelecimento())));
+            assertThat(createdTransacaoDTO.getDescricao().getNsu(), is(equalTo(expectedTransacaoDTO.getDescricao().getNsu())));
+            assertThat(createdTransacaoDTO.getDescricao().getCodigoAutorizacao(), is(equalTo(expectedTransacaoDTO.getDescricao().getCodigoAutorizacao())));
+            assertThat(createdTransacaoDTO.getDescricao().getStatus(), is(equalTo(expectedTransacaoDTO.getDescricao().getStatus())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getId(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getId())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getTipo(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getTipo())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getParcelas(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getParcelas())));
+
+    }
+
+    //Quando a transacao é informada pelo id, a transação é retornada
+    @Test
+    void whenTransactionByIdIsInformedThenIsReturned() throws Exception {
+
+        // Dado
+
+        Long id = 1L;
+
+        //Gera um TransacaoDTO
+        TransacaoDTO expectedTransacaoDTO = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
+
+
+        expectedTransacaoDTO.getDescricao().setNsu("1234567890");
+        expectedTransacaoDTO.getDescricao().setCodigoAutorizacao("147258369");
+        expectedTransacaoDTO.getDescricao().setStatus(StatusEnum.AUTORIZADO);
+
+        //Tranforma o TransacaoDTO em um Transacao
+        Transacao transacao = (Transacao) Mapper.convert(expectedTransacaoDTO, Transacao.class);
+
+        //When
+
+        //transacaoService.findById(id) -> transacao
+        when(transacaoRepository.findById(id))
+                .thenReturn(Optional.ofNullable(transacao));
+
+        // Então
+
+            //Cria um TransacaoDTO
+            TransacaoDTO createdTransacaoDTO  = transacaoService.procurarPeloId(id);
+
+            //Verifica se o atributo id do createdTransacaoDTO é igual ao atributo id do expectedDTO
+            assertThat(createdTransacaoDTO.getId(), is(equalTo(expectedTransacaoDTO.getId())));
+            assertThat(createdTransacaoDTO.getCartao(), is(equalTo(expectedTransacaoDTO.getCartao())));
+            assertThat(createdTransacaoDTO.getDescricao().getId(), is(equalTo(expectedTransacaoDTO.getDescricao().getId())));
+            assertThat(createdTransacaoDTO.getDescricao().getValor(), is(equalTo(expectedTransacaoDTO.getDescricao().getValor())));
+            assertThat(createdTransacaoDTO.getDescricao().getDataHora(), is(equalTo(expectedTransacaoDTO.getDescricao().getDataHora())));
+            assertThat(createdTransacaoDTO.getDescricao().getEstabelecimento(), is(equalTo(expectedTransacaoDTO.getDescricao().getEstabelecimento())));
+            assertThat(createdTransacaoDTO.getDescricao().getNsu(), is(equalTo(expectedTransacaoDTO.getDescricao().getNsu())));
+            assertThat(createdTransacaoDTO.getDescricao().getCodigoAutorizacao(), is(equalTo(expectedTransacaoDTO.getDescricao().getCodigoAutorizacao())));
+            assertThat(createdTransacaoDTO.getDescricao().getStatus(), is(equalTo(expectedTransacaoDTO.getDescricao().getStatus())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getId(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getId())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getTipo(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getTipo())));
+            assertThat(createdTransacaoDTO.getFormaPagamento().getParcelas(), is(equalTo(expectedTransacaoDTO.getFormaPagamento().getParcelas())));
+
+    }
+
+    //Quando a transacao não é informada, todas as transações são retornadas
+    @Test
+    void whenTransactionIsCalledThenAllIsReturned() throws Exception {
+
+        //Dado
+
+        //Gera um TransacaoDTO
+        TransacaoDTO transacaoDTO1 = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
+
+        transacaoDTO1.getDescricao().setNsu("1234567890");
+        transacaoDTO1.getDescricao().setCodigoAutorizacao("147258369");
+        transacaoDTO1.getDescricao().setStatus(StatusEnum.AUTORIZADO);
+
+        //Tranforma o TransacaoDTO em um Transacao
+        Transacao transacao1 = (Transacao) Mapper.convert(transacaoDTO1, Transacao.class);
+
+        //Gera um TransacaoDTO
+        TransacaoDTO transacaoDTO2 = TransacaoDTOBuilder.builder().build().toTransacaoDTO();
+
+        transacaoDTO2.getDescricao().setNsu("1234567890");
+        transacaoDTO2.getDescricao().setCodigoAutorizacao("147258369");
+        transacaoDTO2.getDescricao().setStatus(StatusEnum.AUTORIZADO);
+
+        //Tranforma o TransacaoDTO em um Transacao
+        Transacao transacao2 = (Transacao) Mapper.convert(transacaoDTO2, Transacao.class);
+
+        List<TransacaoDTO> transacaoDTOList = new ArrayList<>();
+        transacaoDTOList.add(transacaoDTO1);
+        transacaoDTOList.add(transacaoDTO2);
+
+        List<Transacao> transacaoList = new ArrayList<>();
+        transacaoList.add(transacao1);
+        transacaoList.add(transacao2);
+
+        //Quando
+
+        //transacaoService.procurarTodos() -> transacaoDTOList
+        when(transacaoRepository.findAll())
+                .thenReturn(transacaoList);
+
+
+        //Cria um TransacaoDTO
+        List<TransacaoDTO> createdTransacaoDTOList  = transacaoService.procurarTodos();
+
+        //Verifica se o atributo id do createdTransacaoDTO é igual ao atributo id do expectedDTO
+
+        for(int i=0; i < transacaoDTOList.size(); i++){
+            assertThat(createdTransacaoDTOList.get(i).getId(), is(equalTo(transacaoDTOList.get(i).getId())));
+            assertThat(createdTransacaoDTOList.get(i).getCartao(), is(equalTo(transacaoDTOList.get(i).getCartao())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getId(), is(equalTo(transacaoDTOList.get(i).getDescricao().getId())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getValor(), is(equalTo(transacaoDTOList.get(i).getDescricao().getValor())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getDataHora(), is(equalTo(transacaoDTOList.get(i).getDescricao().getDataHora())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getEstabelecimento(), is(equalTo(transacaoDTOList.get(i).getDescricao().getEstabelecimento())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getNsu(), is(equalTo(transacaoDTOList.get(i).getDescricao().getNsu())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getCodigoAutorizacao(), is(equalTo(transacaoDTOList.get(i).getDescricao().getCodigoAutorizacao())));
+            assertThat(createdTransacaoDTOList.get(i).getDescricao().getStatus(), is(equalTo(transacaoDTOList.get(i).getDescricao().getStatus())));
+            assertThat(createdTransacaoDTOList.get(i).getFormaPagamento().getId(), is(equalTo(transacaoDTOList.get(i).getFormaPagamento().getId())));
+            assertThat(createdTransacaoDTOList.get(i).getFormaPagamento().getTipo(), is(equalTo(transacaoDTOList.get(i).getFormaPagamento().getTipo())));
+            assertThat(createdTransacaoDTOList.get(i).getFormaPagamento().getParcelas(), is(equalTo(transacaoDTOList.get(i).getFormaPagamento().getParcelas())));
+
+        }
 
     }
 
